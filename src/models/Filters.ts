bd833fe723600f6445f9d75fb4fa493ff5e427af
Matthew_Pfister <matthew.pfister@slalom.com>
2020-01-28 16:18:49 -0600

    updating filters to include primary key for partial searches

--- src/models/Filters.ts
+++ src/models/Filters.ts
@@ -1,8 +1,11 @@
 export interface PlayerFilters {
+  playerId?: string;
   playerName?: string;
   userName?: string;
 }
 
 export interface SeasonFilters {
+  startDate?: string;
+  seasonId?: string;
   isActive?: boolean;
 }
